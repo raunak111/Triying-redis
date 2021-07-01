cd399b02576eebc7117201ce382d41e01511d660
raunak111 <raunak.agarwal341999@gmail.com>
2021-07-01 17:33:06 +0530

    Tried writing LibStreamWriter

--- sources/clients/StreamReadingClient/client.mjs
+++ sources/clients/StreamReadingClient/client.mjs
@@ -13,16 +13,27 @@ adapter = new LibRedisAdapter();
 
 const inst = await adapter.newInstance(ServerConfig);
 
-console.log('adapter online and connected\n');
+console.log('client online and connected\n');
 
 inst.rawCall(['XREAD','STREAMS', 'mystream','0'], function(e, resp){
     console.log('first stream read: ', e, resp[0][0,1], '\n');
 });
 
-//   inst.rawCall(['XADD','mystream','*','message2','My second message in the stream'], function(e, resp){
-//       console.log('second stream id: ', e, resp, '\n');
-//   });
-  
-//   inst.rawCall(['XADD','mystream','*','message3','My third message in the stream'], function(e, resp){
-//       console.log('third stream id: ', e, resp, '\n');
-//   });
+// import {
+//     LibStreamWriter,
+// } from '@coinhaven/libstreamwriter/LibStreamWriter.mjs';
+
+// const Config = {
+//         host: 'localhost',
+//         port: 6380
+// };
+
+// let writer = null;
+
+// writer = new LibStreamWriter();
+
+// const inst = await writer.connectToPort(Config);
+
+// inst.rawCall(['XREAD','STREAMS', 'mystream','0'], function(e, resp){
+//         console.log('first stream read: ', e, resp[0][0,1], '\n');
+// });
--- sources/servers/StreamWriterServer/server.mjs
+++ sources/servers/StreamWriterServer/server.mjs
@@ -1,33 +1,27 @@
 import RedisServer from 'redis-server';
- 
 import {
-  LibRedisAdapter,
-} from '@coinhaven/libredisadapter/LibRedisAdapter.mjs';
+  LibStreamWriter,
+} from '@coinhaven/libstreamwriter/LibStreamWriter.mjs';
 
 const server = new RedisServer(6380);
 
 server.open().then(() => {
+
   console.log("server started");
 });
 
-let adapter = null;
-
-const ServerConfig = {
-    host: 'localhost',
-    port: 6380
+const Config = {
+  host: 'localhost',
+  port: 6380
 };
 
-adapter = new LibRedisAdapter();
+let writer = null;
+  
+writer = new LibStreamWriter();
 
-const inst = await adapter.newInstance(ServerConfig);
+//await writer.write('dfdf','fdfd','fdf');
 
-console.log('adapter online and connected\n');
-
-// console.log('\nchecking connection');
-// inst.rawCall(['ping'], function(e, resp){
-// 	console.log('ping', e, resp);
-// });
-
+const inst = await writer.connectToPort(Config);
 
 inst.rawCall(['XADD','mystream','*','message1','My first message in the stream'], function(e, resp){
 	console.log('first stream id: ', e, resp, '\n');
@@ -40,6 +34,38 @@ inst.rawCall(['XADD','mystream','*','message2','My second message in the stream'
 inst.rawCall(['XADD','mystream','*','message3','My third message in the stream'], function(e, resp){
 	console.log('third stream id: ', e, resp, '\n');
 });
+
+
+// let adapter = null;
+
+// const ServerConfig = {
+//     host: 'localhost',
+//     port: 6380
+// };
+
+// adapter = new LibRedisAdapter();
+
+// const inst = await adapter.newInstance(ServerConfig);
+
+// console.log('adapter online and connected\n');
+
+// console.log('\nchecking connection');
+// inst.rawCall(['ping'], function(e, resp){
+// 	console.log('ping', e, resp);
+// });
+
+
+// inst.rawCall(['XADD','mystream','*','message1','My first message in the stream'], function(e, resp){
+// 	console.log('first stream id: ', e, resp, '\n');
+// });
+
+// inst.rawCall(['XADD','mystream','*','message2','My second message in the stream'], function(e, resp){
+// 	console.log('second stream id: ', e, resp, '\n');
+// });
+
+// inst.rawCall(['XADD','mystream','*','message3','My third message in the stream'], function(e, resp){
+// 	console.log('third stream id: ', e, resp, '\n');
+// });
 
 // adapter.shutDownInstance(inst);
 
